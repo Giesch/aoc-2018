bf4c4ecffcd3720bdca2294150eb07a37963b246
Dan Knutson <dan.knutson@gmail.com>
2019-05-12 22:34:25 -0500

    day 7 part 2

--- src/bin/day_7.rs
+++ src/bin/day_7.rs
@@ -10,6 +10,9 @@ fn main() -> StdResult<()> {
     let solution_one = part_one(input, &tasks)?;
     println!("Part One: {}", solution_one);
 
+    let solution_two = part_two(input, &tasks, 60, 5)?;
+    println!("Part Two: {}", solution_two);
+
     Ok(())
 }
 
@@ -29,6 +32,58 @@ fn part_one(input: &str, tasks: &HashSet<u8>) -> StdResult<String> {
     }
 
     Ok(result)
+}
+
+fn part_two(input: &str, tasks: &HashSet<u8>, wait: i32, workers: usize) -> StdResult<usize> {
+    let deps_map = parse_dependencies(input)?;
+
+    let mut time_taken = 0;
+    let mut workers: Vec<Option<u8>> = vec![None; workers];
+    let mut completed_tasks: HashSet<u8> = HashSet::new();
+    let mut work_remaining: HashMap<u8, i32> = (b'A'..=b'Z')
+        .enumerate()
+        .map(|(i, task)| (task, wait + 1 + i as i32))
+        .collect();
+
+    while completed_tasks != *tasks {
+        time_taken += 1;
+        let available_tasks = available_tasks(&tasks, &deps_map, &completed_tasks);
+        workers = assign_workers(workers, available_tasks);
+
+        for worker in 0..workers.len() {
+            if let Some(task) = workers[worker] {
+                let time = work_remaining.entry(task).or_insert_with(|| panic!("agh"));
+                *time -= 1;
+
+                if *time <= 0 {
+                    workers[worker] = None;
+                    completed_tasks.insert(task);
+                }
+            }
+        }
+    }
+
+    Ok(time_taken)
+}
+
+fn assign_workers(mut workers: Vec<Option<u8>>, available_tasks: HashSet<u8>) -> Vec<Option<u8>> {
+    let worked_tasks: HashSet<_> = workers.iter().filter_map(|task| *task).collect();
+    let mut available_tasks: Vec<_> = available_tasks.difference(&worked_tasks).collect();
+
+    let mut free_workers = vec![];
+    for worker in 0..workers.len() {
+        if let None = workers[worker] {
+            free_workers.push(worker);
+        }
+    }
+
+    for worker in free_workers {
+        if let Some(&task) = available_tasks.pop() {
+            workers[worker] = Some(task);
+        }
+    }
+
+    workers
 }
 
 fn available_tasks(
@@ -88,5 +143,12 @@ mod tests {
         let tasks: HashSet<u8> = (b'A'..=b'F').collect();
         let result = part_one(EXAMPLE_INPUT, &tasks).unwrap();
         assert_eq!(result, "CABDFE".to_string());
+    }
+
+    #[test]
+    fn part_two_example() {
+        let tasks: HashSet<u8> = (b'A'..=b'F').collect();
+        let result = part_two(EXAMPLE_INPUT, &tasks, 0, 2).unwrap();
+        assert_eq!(result, 15);
     }
 }
